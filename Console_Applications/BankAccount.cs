131a087c719e46a9e0afdd7b54b73a0080632357
Manasa Mekala <manasa@gmail.com>
2026-02-11 22:42:37 +0530

    Add project files.

---
+++ Console_Applications/BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Applications
+{
+    internal class BankAccount
+    {
+        private int pin = 2003;
+        private double balance;
+
+        public BankAccount(double initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public bool Login(int enterPin)
+        {
+            return enterPin == pin;
+        }
+        public void CheckBalance()
+        {
+            Console.WriteLine("Current Balance:" + balance);
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new Exception("Entered Invalid Amount!!");
+            }
+            balance += amount;
+            Console.WriteLine("Amount Deposited Successfully");
+        }
+
+        public void withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new Exception("entered invalid Amount!!");
+            }
+            if (amount > balance)
+            {
+                throw new Exception("insufficient Balance");
+            }
+            balance -= amount;
+            Console.WriteLine("Please Collect Your Cash.");
+        }
+    }
+}
---
+++ Console_Applications/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Console_Applications
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(10000);
+            Console.WriteLine("enter Pin: ");
+            int enterPin = Convert.ToInt32(Console.ReadLine());
+            if (!account.Login(enterPin))
+            {
+                Console.WriteLine("InCorrect Pin");
+                return;
+            }
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("\n==================== ATM MENU =========");
+                Console.WriteLine("1.Check Balance");
+                Console.WriteLine("2.Deposit");
+                Console.WriteLine("3.Withdraw");
+                Console.WriteLine("4.Exit");
+                Console.Write("enter Your Choice: ");
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            account.CheckBalance();
+                            break;
+                        case 2:
+                            Console.Write("enter amount to deposit: ");
+                            double dep = Convert.ToDouble(Console.ReadLine());
+                            account.Deposit(dep);
+                            break;
+                        case 3:
+                            Console.Write("enter amount to withdraw:");
+                            double with = Convert.ToDouble(Console.ReadLine());
+                            account.withdraw(with);
+                            break;
+                        case 4:
+                            Console.WriteLine("Thank you for using ATM");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option!");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter numeric values(1-4) only");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            while (choice != 4);
+
+        }
+    }
+}
